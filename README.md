a1862b6fcb1d8455111f090435c98be179abf351
Bartosz Kaznowski <bartekkaznowski@gmail.com>
2019-08-23 22:16:56 +0100

    Minor improvements

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # Party emoji generator
-This tool makes slack party emojis.
+This tool makes slack party emojis. It can convert all files ending with jpg, jpeg, png. It resizes the image to 100x100 so it fits slacks size restrictions.
 
-Before:
+### Before:
+![](/input_data/Portrait_Placeholder.jpg)  
+![](/input_data/download.jpeg)
 
-![](/input_data/Portrait_Placeholder.jpg)
+### After:
 
-After:
+![](/output_data/Portrait_Placeholder.jpg.gif)  
+![](/output_data/download.jpeg.gif)
 
-![](/output_data/Portrait_Placeholder.jpg.gif)
 
 ## Requirements
 - Must have docker installed
 
 ## How to use
 1. Place files in input_data (must end with .jpg)
-2. execute `sudo ./run.sh`. Sudo is required for docker.
+2. Execute `./run.sh`. Sudo may be required for docker.
 3. Retrieve gifs from output_data
+
+## Issues
+* It doesn't handle transparency properly. Currently, it replaces solid black, solid white and transparent pixels with white (no color variation).
--- party_generator.py
+++ party_generator.py
@@ -3,14 +3,47 @@ import numpy as np
 import colorsys
 import glob
 from blend_modes import overlay, multiply
+import itertools
 
 colours = [
     '#ffd68c', '#8cff8c', '#8cffff', '#8cb5ff', '#d68cff', '#ff8cff',
 ]
 
+def replace_color_with_transaprency(img, r, g, b):
+    data = img.getdata()
+
+    newData = []
+    for item in data:
+        if item[0] == r and item[1] == g and item[2] == b:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    return newData
+
+def replace_alpha_with_white(img):
+    data = img.getdata()
+
+    newData = []
+    for item in data:
+        if item[3] == 0:
+            newData.append((255, 255, 255, 255))
+        else:
+            newData.append(item)
+    return newData
+
+
 def make_party_gif(filename):
     print(f'Processing {filename}')
-    im = Image.open(filename).convert('RGBA')
+    im = Image.open(filename).convert('LA').convert('RGBA')
+
+    basewidth = 100
+    wpercent = (basewidth/float(im.size[0]))
+    hsize = int((float(im.size[1])*float(wpercent)))
+    im = im.resize((basewidth,hsize), Image.ANTIALIAS)
+
+    im.putdata(replace_color_with_transaprency(im, 255, 255, 255))
+    # im.putdata(replace_color_with_transaprency(im, 0, 0, 0))
+
     background_img = np.array(im)  # Inputs to blend_modes need to be numpy arrays.
     background_img_float = background_img.astype(float)  # Inputs to blend_modes need to be floats.
 
@@ -21,13 +54,14 @@ def make_party_gif(filename):
         foreground_img_float = foreground_img.astype(float)
 
         # Blend images
-        opacity = 0.7
+        opacity = 0.8
         blended_img_float = overlay(background_img_float, foreground_img_float, opacity)
         blended_img_float = multiply(blended_img_float, foreground_img_float, opacity)
 
         # Convert blended image back into PIL image
         blended_img = np.uint8(blended_img_float)
         blended_img_raw = Image.fromarray(blended_img)
+        blended_img_raw.putdata(replace_alpha_with_white(blended_img_raw))
         frames.append(blended_img_raw)
 
     frames = frames + frames[::-1]
@@ -39,10 +73,16 @@ def make_party_gif(filename):
         format='GIF',
         append_images=frames[1:],
         save_all=True,
-        duration=200,
+        duration=100,
         loop=0
     )
 
+filetypes = [
+    '*.jpg',
+    '*.jpeg',
+    '*.png',
+]
+
 print('Running')
-for name in glob.glob('/input_data/*.jpg'):
+for name in list(itertools.chain.from_iterable([glob.glob(f'/input_data/{f}') for f in filetypes])):
     make_party_gif(name)
